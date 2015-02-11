f093d57ff16dcce56e3c863c3a37093ca6cf3f6e
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-11 00:27:30 -0200

    add header script logic

---
+++ scripts/header.js
@@ -0,0 +1,19 @@
+var header,
+    element,
+    status;
+
+header = function() {
+  element = document.getElementById('header');
+
+  window.onscroll = function(e) {
+    if (!status && document.body.scrollTop > 0) {
+      element.classList.add('toggle');
+      status = true;
+    } else if (status && document.body.scrollTop < 0) {
+      element.classList.remove('toggle');
+      status = false;
+    }
+  }
+};
+
+module.exports = header;
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 var main,
-    styles = require('./styles');
+    styles = require('./styles'),
+    header = require('./header');
 
 main = function() {
   styles();
+  header();
 };
 
 document.addEventListener('DOMContentLoaded', main);
