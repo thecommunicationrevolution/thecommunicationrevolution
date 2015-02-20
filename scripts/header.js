49cf60ffcf15f47131f11d692040de9ee3d92850
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-20 00:27:22 -0200

    set initial responsive style

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,8 @@
 var scroll = require('./scroll'),
     header,
     video,
+    menuButton,
     scrollButton,
-    element,
     status,
     links,
     timer;
@@ -11,6 +11,8 @@ function scrollToSection(e) {
   e.preventDefault();
   var section = document.getElementById(e.currentTarget.getAttribute('href')),
       elementScrollTop = section.offsetTop;
+
+  header.classList.remove('nav-small--open');
 
   if (section.id !== 'landing')
     elementScrollTop -= 65;
@@ -21,30 +23,38 @@ function scrollToSection(e) {
   });
 }
 
-header = function() {
+function toggleMenu(e) {
+  e.preventDefault();
+
+  if (header.classList.contains('nav-small--open'))
+    return header.classList.remove('nav-small--open');
+  header.classList.add('nav-small--open');
+}
+
+module.exports = function() {
   wrapper = document.getElementById('wrapper');
   video = document.getElementById('video');
-  element = document.getElementById('header');
-  links = document.getElementsByTagName('a');
+  header = document.getElementById('header');
+  links = document.querySelectorAll('header nav li a, .nav-small-logo');
   scrollButton = document.getElementById('start-scroll');
-  
-  for (var i = links.length - 1; i >= 0; i--) {
+  menuButton = document.getElementsByClassName('nav-small-button')[0];
+
+    for (var i = links.length - 1; i >= 0; i--) {
     links[i].addEventListener('click', scrollToSection);
   };
 
+  menuButton.addEventListener('click', toggleMenu);
   scrollButton.addEventListener('click', scrollToSection);
 
   window.onscroll = function(e) {
     if (!status && window.scrollY > 25) {
-      element.classList.add('toggle');
+      header.classList.add('toggle');
       video.pause();
       status = true;
     } else if (status && window.scrollY < 25) {
-      element.classList.remove('toggle');
+      header.classList.remove('toggle');
       video.play();
       status = false;
     }
   }
 };
-
-module.exports = header;
